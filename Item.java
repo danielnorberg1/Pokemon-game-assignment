37c2b765d1bf8698b7fa2ae951b1119e9cf4f63f
eddedomer <31513157+eddedomer@users.noreply.github.com>
2023-09-20 14:54:48 +0200

    Update Item.java

---
+++ Item.java
@@ -0,0 +1,33 @@
+public class Item {
+
+    private String itemName;
+    private int healingPower;
+    private double weight;
+
+    public Item (String itemName, int healingPower, double weight) {
+    this.itemName = itemName;
+    this.healingPower = healingPower;
+    this.weight = weight;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getHealingPower () {
+        return healingPower;
+    }
+
+    public double getWeight() {
+        return weight;
+
+
+
+    }
+
+    enum HealingResult {
+        FULL_HP ("%s could not use %s. HP is already full.");
+        HEAL_AMOUNT (   )
+        CANTUSE (    )
+
+}
